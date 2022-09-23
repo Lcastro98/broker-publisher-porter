92f68003bc4431aab8bffdf741423f9b94fc44d4
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-09-23 17:29:58 -0500

    initial commit

---
+++ src/main/java/com/sofka/broker/BrokerPublisherPorterApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerPublisherPorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerPublisherPorterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/broker/BrokerPublisherPorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerPublisherPorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
