3642948be1ea18cc864880b886eaf29c398763e5
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-09-25 21:42:12 -0500

    Se agrega el controlador para enviar los mensajes

--- src/main/java/com/sofka/broker/controller/RabbitMQTopicWebController.java
+++ src/main/java/com/sofka/broker/controller/RabbitMQTopicWebController.java
@@ -1,2 +1,24 @@
-package com.sofka.broker.controller;public class RabbitMQTopicWebController {
+package com.sofka.broker.controller;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/broker/topic")
+public class RabbitMQTopicWebController {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @GetMapping(value = "/producer")
+    public String producer(@RequestParam("exchangeName") String exchange, @RequestParam("routingKey") String routingKey,
+                           @RequestParam("messageData") String messageData){
+        amqpTemplate.convertAndSend(exchange, routingKey, messageData);
+
+        return "Message sent to the RabbitMQ topic exchange Succesfully";
+    }
 }
